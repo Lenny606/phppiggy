21790b94fff49fc09db6f33e8e63f2d04ed15d43
Lenny606 <92226943+Lenny606@users.noreply.github.com>
2024-01-08 22:01:06 +0100

    refactor database connection

--- cli.php
+++ cli.php
@@ -2,23 +2,39 @@
 
 //DNS creation - DATA SOURCE NAME
 //driver:key=value format
+//REFACTOR ===> LOGIC MOVED TO Database.php
 
-$driver = 'mysql'; //compatible with mariaDB
+//$driver = 'mysql'; //compatible with mariaDB
+//
+////http_build_query function builds url, can be used DNS string to
+//$config = http_build_query([
+//    'host' => 'localhost', //key value pair
+//    'port' => 3306,
+//    'dbname' => 'phppiggy'
+//], arg_separator: ';');
+//
+//$dsn = "{$driver}:{$config}";
+//$username = 'root';
+//$password = '';
+//
+//try{
+//    $db = new PDO($dsn, $username, $password);
+//} catch (PDOException $e) {
+//    die("Could not connect to dtb"); //stops the script
+//}
+//echo "connected to database";
 
-//http_build_query function builds url, can be used DNS string to
-$config = http_build_query([
-    'host' => 'localhost', //key value pair
-    'port' => 3306,
-    'dbname' => 'phppiggy'
-], arg_separator: ';');
+include __DIR__ . "/src/Framework/Database.php";
 
-$dsn = "{$driver}:{$config}";
-$username = 'root';
-$password = '';
+use \Framework\Database;
 
-try{
-    $db = new PDO($dsn, $username, $password);
-} catch (PDOException $e) {
-    die("Could not connect to dtb"); //stops the script
-}
+//use the class
+
+$db = new Database(
+    'mysql',
+    ['host' => 'localhost','port' => 3306, 'dbname' => 'phppiggy'],
+    'root',
+    ""
+);
+
 echo "connected to database";
