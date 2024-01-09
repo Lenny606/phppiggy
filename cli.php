ec0b4e044c0c6660606416329ae5f1cbba197569
Lenny606 <92226943+Lenny606@users.noreply.github.com>
2024-01-09 21:28:12 +0100

    prepared stmt

--- cli.php
+++ cli.php
@@ -42,6 +42,16 @@ echo "connected to database";
 $query = "SELECT * FROM products";
 //save return value, many fetch modes available - FETCH_NUM, FETCH_
 $stmt = $db->connection->query($query, PDO::FETCH_ASSOC);
+$query = "SELECT * FROM products WHERE name =:name";
+//prepared statement for safe queries
+$stmt2 = $db->connection->prepare($query);
+
+//bind method sets parameters without executing, query can be executed later
+$stmt2->bindValue(':name', 'hat', PDO::PARAM_STR);
+//execute with parameters
+$stmt2->execute([
+    'name' => "hat"
+]);
 
 //here fetch mode overrides fetch mode above, this is normal
 var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
