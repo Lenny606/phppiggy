bb94a9a14c9146cededdd3fca08649925ec3ade5
Lenny606 <thomas.kravcik@gmail.com>
2024-01-13 16:20:04 +0100

    env file

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use App\Services\ValidatorService;
+use Framework\Database;
 use Framework\TemplateEngine;
 use App\Config\Paths;
 
@@ -11,5 +12,15 @@ use App\Config\Paths;
 //keys in array are most important and have to be unique
 return [
     TemplateEngine::class => fn() => new TemplateEngine(Paths::VIEW),
-    ValidatorService::class => fn() => new ValidatorService()
+    ValidatorService::class => fn() => new ValidatorService(),
+    Database::class => fn() => new Database(
+        $_ENV['DB_DRIVER'],
+        [
+            $_ENV['DB_HOST'],
+            $_ENV['DB_USER'],
+            $_ENV['DB_NAME']
+        ],
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASSWORD']
+    )
 ];
